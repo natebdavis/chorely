d161ad4af2d4333b6b4bc6fbfb9a87a5094b7129
jlm765 <jlm765@scarletmail.rutgers.edu>
2026-03-03 22:07:35 -0500

    Changed Text Color

--- frontend/app/(tabs)/leaderboard.tsx
+++ frontend/app/(tabs)/leaderboard.tsx
@@ -10,5 +10,5 @@ export default function Leaderboard() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  text: { fontSize: 20, fontWeight: "bold" },
+  text: { fontSize: 20, fontWeight: "bold", color: "white"  },
 });
