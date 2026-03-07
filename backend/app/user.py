a868414fa0c01e0c03ab4684c7f5bca8db72db3e
aberlins <aberlinski98@gmail.com>
2026-03-07 18:35:28 -0400

    Worked on user and chore modules, finishing up classes for each one.

--- backend/app/user.py
+++ backend/app/user.py
@@ -1,6 +1,51 @@
+from typing import Optional
+import re
+
+"""
+Module for managing User operations.
+Contributers: Gilligan Berlinski
+"""
 class User:
+    """User."""
 
-    name: str
+    username: str
+    fname: str
+    lname: str
+    phone_num: Optional[int]
+    """Optional information, user can attach their phone number to their account."""
+    _EMAIL_REGULAR_EXPRESSION = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}"
+    """Regular expression for validating email."""
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, username: str, fname: str, lname: str, email: str, 
+                 phone_num: Optional[int] = None):
+        """Constructor for User Class.
+        Inputs: `email` will be checked if it is a valid email, if not then it is set to null.
+        `phone_num` can be null or have a value upon creation of the user object.
+        Output: `User` Object
+        """
+        self.username = username
+        self.fname = fname
+        self.lname = lname
+        self.phone_num = phone_num
+        self.email = email if self.__isValidEmail(email) else None
+
+    @property
+    def email(self) -> str:
+        return self._email
+
+    @email.setter
+    def email(self, email) -> bool:
+        """Setter for `email` if a valid email is given change the email to the new one, if not keep
+        the original email.
+        Output: `True` if was able to change email to new value, `False` if otherwise."""
+        if self.__isValidEmail(email):
+            self._email = email
+            return True
+        else:
+            return False
+        
+    def __isValidEmail(self, email: str) -> bool:
+         """Private Method for checking if email given is in a valid format.
+         Output: `True` if email is valid, `False` if otherwise."""
+         valid_email = re.match(self._EMAIL_REGULAR_EXPRESSION, email)
+         return True if valid_email else False
