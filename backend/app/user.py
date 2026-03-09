ba4014b40260ba2e6fa2613f3eaf4a9ad7087ec8
Gilligan Berlinski <gilololol64@gmail.com>
2026-03-09 00:02:11 -0300

    Needed ton Commit directly from github

--- backend/app/user.py
+++ backend/app/user.py
@@ -2,15 +2,25 @@ from typing import Optional
 import re
 from collections.abc import Iterable
 from typing import Set
-
-from backend.app.chore import Chore, Notification
-
+from misc import CreateFromDict
+from enum import Enum
 
 """
 Module for managing User operations.
 Contributers: Gilligan Berlinski, Nathaniel Davis
 """
-class User:
+
+class User_Col_Name(Enum):
+    userid = "userid"
+    passhash = "passhash"
+    username = "username"
+    fname = "fname"
+    lname = "lname"
+    email = "email"
+    phone = "phone"
+    householdid = "householdid"
+
+class User(CreateFromDict):
     """User."""
 
     username: str
@@ -18,10 +28,10 @@ class User:
     lname: str
     phone_num: Optional[int]
     """Optional information, user can attach their phone number to their account."""
-    _EMAIL_REGULAR_EXPRESSION = "[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}"
+    _EMAIL_REGULAR_EXPRESSION = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,7}"
     """Regular expression for validating email."""
 
-    def __init__(self, username: str, fname: str, lname: str, email: str, 
+    def __init__(self, username: str, userid: int, fname: str, lname: str, email: str, 
                  phone_num: Optional[int] = None):
         """Constructor for User Class.
         Inputs: `email` will be checked if it is a valid email, if not then it is set to null.
@@ -29,17 +39,31 @@ class User:
         Output: `User` Object
         """
         self.username = username
+        self.userid = userid
         self.fname = fname
         self.lname = lname
         self.phone_num = phone_num
         self.email = email if self.__isValidEmail(email) else None
+
+    @classmethod
+    def from_dict(cls, user_dict: dict):
+        """Alternate Constructor for `User`
+        Input: Dictionary with all values assoicated with a `User`"""
+        username = user_dict[User_Col_Name.username.value]
+        userid = user_dict[User_Col_Name.userid.value]
+        fname = user_dict[User_Col_Name.fname.value]
+        lname = user_dict[User_Col_Name.lname.value]
+        email = user_dict[User_Col_Name.email.value]
+        phone = user_dict[User_Col_Name.phone.value]
+
+        return cls(username, userid, fname, lname, email = email, phone_num = phone)
 
     @property
     def email(self) -> str:
         return self._email
 
     @email.setter
-    def email(self, email: str) -> bool:
+    def email(self, email) -> bool:
         """Setter for `email` if a valid email is given change the email to the new one, if not keep
         the original email.
         Output: `True` if was able to change email to new value, `False` if otherwise."""
@@ -54,15 +78,3 @@ class User:
          Output: `True` if email is valid, `False` if otherwise."""
          valid_email = re.match(self._EMAIL_REGULAR_EXPRESSION, email)
          return True if valid_email else False
-
-def get_chores(user: User, chores: Iterable[Chore]) -> Set[Chore]:
-    """Get all chores that a user is assigned to"""
-    return {chore for chore in chores if chore.assignee == user}
-
-def get_notifs(user: User, chores: Iterable[Chore]) -> Set[Notification]:
-    """Get all notifications for chores that a user is assigned to"""
-    notifications: Set[Notification] = set()
-    for chore in chores:
-        if chore.assignee == user:
-            notifications.update(chore.notifications)
-    return notifications
