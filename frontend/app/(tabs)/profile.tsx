72539c7727a6de6b20e6b21b92bc305e71d2656d
jlm765 <jlm765@scarletmail.rutgers.edu>
2026-03-11 13:44:01 -0400

    Updated the Profile Tab

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,15 +1,88 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 
-export default function Profile() {
+export default function ProfileScreen() {
+  const username = "Username";
+
   return (
-     <View style={styles.container}>
-          <Text style={styles.text}>My Profile</Text>
-        </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Profile</Text>
+
+      <View style={styles.avatarWrapper}>
+        <Image
+          source={require("../../assets/images/default_profile.png")}
+          style={styles.avatar}
+        />
+      </View>
+
+      <TouchableOpacity 
+        style={styles.settingsButton}
+      >
+        <Ionicons name="settings-outline" size={20} color="#fff" />
+        <Text style={styles.buttonText}>Settings</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+        style={styles.logoutButton}
+        onPress={() => router.replace("/login")}
+      >
+        <Ionicons name="log-out-outline" size={20} color="#fff" />
+        <Text style={styles.buttonText}>Logout</Text>
+
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  text: { fontSize: 20, fontWeight: "bold", color: "white"  },
+  container: {
+    flex: 1,
+    backgroundColor: "#111",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 24,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 28,
+    fontWeight: "700",
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  avatarWrapper: {
+    marginBottom: 40,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 65,
+    backgroundColor: "#333",
+  },
+  settingsButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#2a2a2a",
+    width: "80%",
+    paddingVertical: 14,
+    borderRadius: 14,
+    marginBottom: 16,
+    gap: 8,
+  },
+  logoutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#004fb0",
+    width: "80%",
+    paddingVertical: 14,
+    borderRadius: 14,
+    gap: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
 });
